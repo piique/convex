4cd83d5775feb89f09c88e3af62fbc58b9d6fcef
Pedro Vilaça <pedrovilacav@gmail.com>
2020-05-05 00:04:46 -0300

    Primeiro commit

---
+++ conversor/scrip_python.py
@@ -0,0 +1,31 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+def pdf_to_text(path):
+    manager = PDFResourceManager()
+    retstr = StringIO()
+    layout = LAParams(all_texts=True)
+    device = TextConverter(manager, retstr, laparams=layout)
+    filepath = open(path, 'rb')
+    interpreter = PDFPageInterpreter(manager, device)
+
+    for page in PDFPage.get_pages(filepath, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    filepath.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+if __name__ == "__main__":
+    text = pdf_to_text("entrada.pdf")
+    arquivo = open('saida_xml.doc','r+')
+    arquivo.seek(0)
+    arquivo.write(text)
+    arquivo.close()
